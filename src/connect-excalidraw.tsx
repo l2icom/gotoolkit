37bcb385b2c64e793e89b5b03eacc3ddf7f1079d
Quang T <quangfr@users.noreply.github.com>
2025-12-09 15:17:48 +0100

    fix scrol

--- src/connect-excalidraw.tsx
+++ src/connect-excalidraw.tsx
@@ -109,7 +109,6 @@ class ExcalidrawBridge {
                         <ExcalidrawAny
                             excalidrawAPI={syncApi}
                             theme="light"
-                            detectScroll={false}
                             viewModeEnabled={false}
                             gridModeEnabled={false}
                             zenModeEnabled={false}
